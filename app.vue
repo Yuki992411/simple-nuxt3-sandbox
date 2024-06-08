879b8766378a192b51a21e705d5bb6af60f17198
Yuki992411 <k.yuki.9924.11@gmail.com>
2024-06-08 13:42:56 +0900

    [add] use i18n (en, ja)

--- app.vue
+++ app.vue
@@ -1,5 +1,12 @@
 <template>
   <div>
-    <NuxtWelcome />
+    <button @click="setLocale('en')">en</button>
+    <button @click="setLocale('ja')">ja</button>
+    <p>{{ locale }}</p>
+    <p>{{ $t("titleMax", { max: 4 }) }}</p>
   </div>
 </template>
+
+<script setup lang="ts">
+const { locale, setLocale } = useI18n();
+</script>
